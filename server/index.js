50c3eff6eaf3cdbf1e7c6d1cae303b6d39f3fdd2
Bradley Tudor <bradley@stewartfilm.com>
2018-08-23 15:04:03 -0700

    added socket io

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
-const app = require('./server');
+const {server} = require('./server');
+
 const port = process.env.PORT || 3000;
 
 
 
 
-app.listen(port,()=>{
+server.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
 })
--- server/server.js
+++ server/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+
+const http = require('http');
+const socketIO = require('socket.io');
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
+
+
 
 app.use(express.static(publicPath));
 //app.set('view engine', 'html');
@@ -13,5 +21,8 @@ routes(app);
 
 
 
-module.exports = app;
+module.exports = {
+    app,
+    server
+}
 
